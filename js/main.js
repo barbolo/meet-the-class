ac6acc514c2e70f82ae86155fc6c726e639beb3d
Kersul <joaohkfaria@gmail.com>
2014-03-18 20:35:36 -0300

    Adding personal information for Joao Henrique Kersul Faria

--- js/main.js
+++ js/main.js
@@ -74,7 +74,12 @@ people = [
   },
   {
     full_name: 'João Henrique Kersul Faria',
-    first_name: 'João Henrique'
+    first_name: 'João Henrique',
+    course: 'Curso Semestral',
+    birthday: '07/10/1992',
+    about: 'Ficar das 18:00 até as 21:30 em um curso de programação já define muito sobre você.',
+    picture: 'e6819752a3cfb898f155e2228504ec98'
+    
   },
   {
     full_name: 'Lucas Batista Gabriel',
